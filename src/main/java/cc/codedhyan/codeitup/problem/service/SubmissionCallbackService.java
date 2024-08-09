2827a5d8f98a91795cf4649b1f0f0be41febfefb
Bijit Mondal <bijitmondal3011@rediffmail.com>
2024-08-09 10:40:38 +0530

    Pushing with issue in callback service

--- src/main/java/cc/codedhyan/codeitup/problem/service/SubmissionCallbackService.java
+++ src/main/java/cc/codedhyan/codeitup/problem/service/SubmissionCallbackService.java
@@ -39,18 +39,21 @@ public class SubmissionCallbackService {
         log.info("Pending test cases: {}", pendingTestCases.size());
         log.info("Failed test cases: {}", failedTestCases.size());
         log.info("All test cases: {}", allTestCaseData.size());
-        if (pendingTestCases.isEmpty()) {
+
+        // If there are no pending test cases, then the submission is complete
+        // Some issue that I am facing is pendingTestCases doesn't become 0 even after all test cases are completed
+//      // It is remaining 1 even after all test cases are completed
+        if (pendingTestCases.size() == 1) {
             boolean accepted = failedTestCases.isEmpty();
             Submission submission = submissionRepository.findById(testCase.getSubmissionId())
                     .orElseThrow(() -> new ApiRequestExceptionNotFound("Submission not found"));
 
             submission.setSubmissionResult(accepted ? SubmissionResult.ACCEPTED : SubmissionResult.REJECTED);
             log.info("Submission result: {}", submission.getSubmissionResult());
-//            Memory and time returning null, why?
-//            submission.setTime(allTestCaseData.stream()
-//                    .mapToDouble(tc -> Double.parseDouble(tc.getTime()))
-//                    .max().orElse(0));
-//            submission.setMemory(allTestCaseData.stream().mapToInt(TestCases::getMemory).max().orElse(0));
+            submission.setTime(allTestCaseData.stream()
+                    .mapToDouble(tc -> Double.parseDouble(tc.getTime()))
+                    .max().orElse(0));
+            submission.setMemory(allTestCaseData.stream().mapToInt(TestCases::getMemory).max().orElse(0));
             submissionRepository.save(submission);
         }
     }
