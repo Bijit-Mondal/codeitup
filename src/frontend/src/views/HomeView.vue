0c33713cf3cd3b1d9096913569e1ea7ed053c7ea
Bijit Mondal <68532969+Bijit-Mondal@users.noreply.github.com>
2024-11-28 15:31:05 +0000

    Home Page Update

--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,3 +1,30 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+});
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+}
+.left-half {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
 <template>
-  Home
+  <vs-row class="container">
+    <vs-col class="left-half">
+      <object class="no-pointers" data="https://bijit.xyz/cockpit-master/storage/uploads/2024/11/28/FeaturePreviewRealTimeNew_uid_674888e721374.svg">Banner image</object>
+    </vs-col>
+    <vs-col>
+      Home Component
+    </vs-col>
+  </vs-row>
 </template>
