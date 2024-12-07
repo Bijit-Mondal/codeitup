90c78405546dfbf0ea90fc4efef1b045dda224ea
Bijit Mondal <68532969+Bijit-Mondal@users.noreply.github.com>
2024-12-07 14:56:43 +0000

    Commenting for deploy testing

--- src/main/java/cc/codedhyan/codeitup/problem/controller/SubmissionCallbackController.java
+++ src/main/java/cc/codedhyan/codeitup/problem/controller/SubmissionCallbackController.java
@@ -1,25 +1,25 @@
-package cc.codedhyan.codeitup.problem.controller;
+// package cc.codedhyan.codeitup.problem.controller;
 
-import cc.codedhyan.codeitup.problem.Judge0Response;
-import cc.codedhyan.codeitup.problem.service.SubmissionCallbackService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+// import cc.codedhyan.codeitup.problem.Judge0Response;
+// import cc.codedhyan.codeitup.problem.service.SubmissionCallbackService;
+// import jakarta.validation.Valid;
+// import lombok.RequiredArgsConstructor;
+// import lombok.extern.slf4j.Slf4j;
+// import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/v1/open/submission-callback")
-@Slf4j
-@CrossOrigin("*")
-public class SubmissionCallbackController {
-    private final SubmissionCallbackService submissionCallbackService;
+// @RestController
+// @RequiredArgsConstructor
+// @RequestMapping("/api/v1/open/submission-callback")
+// @Slf4j
+// @CrossOrigin("*")
+// public class SubmissionCallbackController {
+//     private final SubmissionCallbackService submissionCallbackService;
 
-    @PutMapping("")
-    public void submissionCallback(
-            @RequestBody @Valid Judge0Response response
-    ){
-        log.info("Submission callback received: {}", response);
-        submissionCallbackService.processSubmissionCallback(response);
-    }
-}
+//     @PutMapping("")
+//     public void submissionCallback(
+//             @RequestBody @Valid Judge0Response response
+//     ){
+//         log.info("Submission callback received: {}", response);
+//         submissionCallbackService.processSubmissionCallback(response);
+//     }
+// }
